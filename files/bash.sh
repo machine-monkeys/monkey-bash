5ff3c7a68effa898a6d7811eccc93d29fbd52a8f
CurtisGino <curtis.gino@rincon.com>
2026-01-29 08:45:24 -0700

    refactor: ansi escape functions

--- files/bash.sh
+++ files/bash.sh
@@ -7,17 +7,17 @@ fg256color() {
 bg256color() {
     printf "\[\033[48;5;${1}m\]"
 }
-color8_16() {
-    printf "\[\033[${1}m\]"
+ansi_esc() {
+    printf "\[\033[${1}\]"
 }
 
-RED=196
+RED196=196
 WHITE=15
-DGRAY=250
-RESET="\[\033[0m\]"
+GRAY=250
 YELLOW=220
 BLUE27=27
 BLUE32=32
+PURPLE=99
 ORANGE=208
 OG_WHITE=37
 
@@ -38,35 +38,39 @@ DFLAG=$'\u0394'
 #if [[ $- == *i* ]]; then
 #    CMOD=$(( $(date +%s) % 5 ))
 
-C1=$(fg256color 15)
-C2=$(fg256color 32)
-C3=$(fg256color 99)
-C4=$(fg256color 250)
+ACCENT_CLR=$(fg256color "$WHITE")
+USER_CLR=$(fg256color "$BLUE32")
+HOST_CLR=$(fg256color "$PURPLE")
+WDIR_CLR=$(fg256color "$GRAY")
+REPO_CLR=$(fg256color "$ORANGE")
+VENV_CLR=$(fg256color "$YELLOW")$(bg256color "$BLUE27")
+ERR_CLR=$(fg256color "$RED196")
+RESET=$(ansi_esc "0m")
 
 prompt() {
     EC=$?
 
     if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
         b=$(git symbolic-ref --quiet --short HEAD 2>/dev/null) || b=""
-        [[ -n "$b" ]] && F1="${ORANGE}${b}"
+        [[ -n "$b" ]] && F1="${REPO_CLR}${b}"
         if ! git diff --quiet --ignore-submodules -- || ! git diff --quiet --cached --ignore-submodules --; then
-            F1+="${RWHITE}${DFLAG}"
+            F1+="${ACCENT_CLR}${DFLAG}"
         fi
     elif [[ -n ${VIRTUAL_ENV-} ]]; then 
         F1="${YB}${VIRTUAL_ENV_PROMPT:-(${VIRTUAL_ENV##*/})}"
     else
-        F1="${RWHITE}\\A"
+        F1="${ACCENT_CLR}\\A"
     fi
 
-    TOP="${C1}${DR_RND}${L_SEP}${F1}${RESET}${VL}${C2}\\u${RESET}${C1}@${RESET}${C3}\\h${RESET}${C1}:${RESET}${C4}\\W${RESET}${C1}${VL}\\$"
+    TOP="${ACCENT_CLR}${DR_RND}${L_SEP}${F1}${RESET}${VL}${USER_CLR}\\u${RESET}${ACCENT_CLR}@${RESET}${HOST_CLR}\\h${RESET}${ACCENT_CLR}:${RESET}${WDIR_CLR}\\W${RESET}${ACCENT_CLR}${VL}\\$"
 
     if [[ "$EC" != 0 ]]; then
-        TOP+="${RED} ${EC}${RESET}\\n"
+        TOP+="${ERR_CLR} ${EC}${RESET}\\n"
     else
         TOP+="\\n"
     fi
 
-    BOT="${C1}${UR_RND}${HL}${HL}${HL}${R_TRI}${RESET} "
+    BOT="${ACCENT_CLR}${UR_RND}${HL}${HL}${HL}${R_TRI}${RESET} "
     PS1="${TOP}${BOT}"
 }
 PROMPT_COMMAND='prompt'
