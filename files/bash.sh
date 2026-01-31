e1f96b74290fa761a284878d192074699de2be3a
Gino Curtis <ginocurtis5@gmail.com>
2026-01-30 22:28:53 -0700

    feat: color randomization and calculate secondary colors and set RAINBOW_MODE

--- files/bash.sh
+++ files/bash.sh
@@ -2,8 +2,87 @@
 printf '\e[1 q' # Shape
 printf '\e]12;#D0D0D0\a' # Color
 
+# Notes
+# 1. random select dominant color
+# 2. random select dominant major or minor
+# 3. random select 1 of 3 patterns DML, DMM, DLL
+# 4. random select major or minor for secondary colors
+
 # Helper Functions
-fg256color() {
+coin_flip() {
+    case $((RANDOM % 2)) in
+        0) printf '%s' "$1" ;;
+        1) printf '%s' "$2" ;;
+    esac
+}
+tri_flip() {
+    case $((RANDOM % 3)) in
+        0) printf '%s' "$1" ;;
+        1) printf '%s' "$2" ;;
+        2) printf '%s' "$3" ;;
+    esac
+}
+mag_flip() {
+  case $((RANDOM % 5)) in
+    0) printf '%s' 1 ;;
+    1) printf '%s' 6 ;;
+    2) printf '%s' 36 ;;
+    3) printf '%s' 72 ;;
+    4) printf '%s' 108 ;;
+  esac
+}
+calc216() {
+    local R=$1 G=$2 B=$3 calc
+    calc=$((16 + 36*R + 6*G + B))
+    printf '%s' "$calc"
+}
+random_color1() {
+    RANDOM=$(( $(date +%s%N) % 32768 ))
+    DOM=$(tri_flip "red" "green" "blue")
+    PATTERN=$(tri_flip "dml" "dmm" "dll")
+    DOM_VAL=$(coin_flip 4 5)
+    
+    if [[ "$PATTERN" == "dml" ]]; then
+        MED_VAL=$(coin_flip 2 3)
+        LOW_VAL=$(coin_flip 0 1)
+        case "$DOM" in
+            red) if [[ $(coin_flip 0 1) == 0 ]]; then calc216 "$DOM_VAL" "$MED_VAL" "$LOW_VAL"; else calc216 "$DOM_VAL" "$LOW_VAL" "$MED_VAL"; fi ;;
+            green) if [[ $(coin_flip 0 1) == 0 ]]; then calc216 "$MED_VAL" "$DOM_VAL" "$LOW_VAL"; else calc216 "$LOW_VAL" "$DOM_VAL" "$MED_VAL"; fi ;; 
+            blue) if [[ $(coin_flip 0 1) == 0 ]]; then calc216 "$MED_VAL" "$LOW_VAL" "$DOM_VAL"; else calc216 "$LOW_VAL" "$MED_VAL" "$DOM_VAL"; fi ;;
+        esac
+
+    elif [[ "$PATTERN" == "dmm" ]]; then
+        MED_VAL1=$(coin_flip 2 3)
+        MED_VAL2=$(coin_flip 2 3)
+        case "$DOM" in
+            red) calc216 "$DOM_VAL" "$MED_VAL1" "$MED_VAL2" ;;
+            green) calc216 "$MED_VAL1" "$DOM_VAL" "$MED_VAL2" ;;
+            blue) calc216 "$MED_VAL1" "$MED_VAL2" "$DOM_VAL" ;;
+        esac
+    else
+        LOW_VAL1=$(coin_flip 0 1)
+        LOW_VAL2=$(coin_flip 0 1)
+        case "$DOM" in
+            red) calc216 "$DOM_VAL" "$LOW_VAL1" "$LOW_VAL2" ;;
+            green) calc216 "$LOW_VAL1" "$DOM_VAL" "$LOW_VAL2" ;;
+            blue) calc216 "$LOW_VAL1" "$LOW_VAL2" "$DOM_VAL" ;;
+        esac
+    fi
+}
+random_color2() {
+    local COLOR1=$1 mag dir op opcheck COLOR2
+    mag=$(mag_flip)
+    dir=$(coin_flip -1 1)
+    op=$(( mag * dir ))
+    opcheck=$(( COLOR1 + op ))
+    if (( opcheck < 16 || opcheck > 231 )); then
+        COLOR2=$(( COLOR1 - op ))
+    else
+        COLOR2=$opcheck
+    fi
+    printf '%s' "$COLOR2"
+}
+fg216() {
     if [[ -n "${2-}" ]]; then
         mode="${2};"
     else
@@ -11,7 +90,7 @@ fg256color() {
     fi
     printf "\[\033[${mode}38;5;${1}m\]"
 }
-bg256color() {
+bg216() {
     if [[ -n "${2-}" ]]; then
         mode="${2};"
     else
@@ -42,75 +121,31 @@ REVERSE=7
 HIDDEN=8
 STRIKETHRU=9
 
-# 256 Color Codes
-BLACK16=16
-BLUE17=17
-BLUE27=27
-BLUE32=32
-BLUE44=44
-BLUE45=45
-BLUE73=73
-BROWN94=94
-BROWN95=95
-GRAY232=232
-GRAY234=234
-GRAY235=235
-GRAY245=245
-GRAY246=246
-GRAY250=250
-GRAY252=252
-GREEN10=10
-GREEN22=22
-GREEN23=23
-GREEN30=30
-GREEN29=29
-GREEN48=48
-GREEN108=108
-ORANGE130=130
-ORANGE172=172
-ORANGE208=208
-PINK163=163
-PINK212=212
-PURPLE93=93
-PURPLE99=99
-RED160=160
-RED196=196
-RED198=198
-WHITE15=15
-YELLOW100=100
-YELLOW184=184
-YELLOW220=220
+C1=$(random_color1)
+C2=$(random_color2 "$C1")
 
-# Use UNIX Epoch for random selection
-if [[ $- == *i* ]]; then
-    CLR_MOD=$(( $(date +%s) % 8 ))
-fi
+FLIP=$(coin_flip 0 1)
 
-# Pick color based on epoch time result
-case "$CLR_MOD" in
-    "0") UC="$BLUE32" HC="$PURPLE99" ;;
-    "1") UC="$GREEN29" HC="$BROWN94" ;;
-    "2") UC="$YELLOW100" HC="$GREEN30" ;;
-    "3") UC="$BLUE44" HC="$PINK212" ;;
-    "4") UC="$BLUE73" HC="$GREEN108" ;;
-    "5") UC="$RED160" HC="$YELLOW184" ;;
-    "6") UC="$PURPLE93" HC="$RED198" ;;
-    "7") UC="$BLUE45" HC="$ORANGE172" ;;
+case "$FLIP" in
+    0) UC="$C1" HC="$C2" ;;
+    1) UC="$C2" HC="$C1" ;;
 esac
 
-ACCENT_CLR=$(fg256color "$WHITE15")
-USER_CLR=$(fg256color "$UC" "$BOLD")
-HOST_CLR=$(fg256color "$HC" "$BOLD")
-WDIR_CLR=$(fg256color "$GRAY250" "$BOLD")
-REPO_CLR=$(fg256color "$BLACK16" "$BOLD")$(bg256color "$GRAY250")
-DFLAG_CLR=$(fg256color "$BLACK16" "$BOLD")
-VENV_CLR=$(fg256color "$YELLOW220")$(bg256color "$BLUE27")
-ERR_CLR=$(fg256color "$RED196")
+ACCENT_CLR=$(fg216 15)
+USER_CLR=$(fg216 "$UC" "$BOLD")
+HOST_CLR=$(fg216 "$HC" "$BOLD")
+WDIR_CLR=$(fg216 250 "$BOLD")
+REPO_CLR=$(fg216 250 "$BOLD")
+DFLAG_CLR=$(fg216 196 "$BOLD")
+VENV_CLR=$(fg216 220)$(bg216 27)
+ERR_CLR=$(fg216 196)
 RESET=$(ansi_esc "0m")
 
-prompt() {
-    EC=$?
+RAINBOW_MODE=true
 
+prompt() {
+    local EC=$? b
+    F1=""
     if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
         b=$(git symbolic-ref --quiet --short HEAD 2>/dev/null) || b=""
         [[ -n "$b" ]] && F1="${REPO_CLR}${b}"
@@ -118,14 +153,21 @@ prompt() {
             F1+="${DFLAG_CLR}${DFLAG}"
         fi
     elif [[ -n ${VIRTUAL_ENV-} ]]; then 
-        F1="${YB}${VIRTUAL_ENV_PROMPT:-(${VIRTUAL_ENV##*/})}"
+        F1="${VENV_CLR}${VIRTUAL_ENV_PROMPT:-(${VIRTUAL_ENV##*/})}"
     else
         F1="${ACCENT_CLR}\\A"
     fi
 
+    if $RAINBOW_MODE; then
+        C1=$(random_color1)
+        C2=$(random_color2 "$C1")
+        USER_CLR=$(fg216 "$C1" "$BOLD")
+        HOST_CLR=$(fg216 "$C2" "$BOLD")
+    fi
+
     TOP="${ACCENT_CLR}${DR_RND}${L_SEP}${F1}${RESET}${VL}${USER_CLR}\\u${RESET}${ACCENT_CLR}@${RESET}${HOST_CLR}\\h${RESET}${ACCENT_CLR}:${RESET}${WDIR_CLR}\\W${RESET}${ACCENT_CLR}${VL}\\$"
 
-    if [[ "$EC" != 0 ]]; then
+    if (( EC != 0 )); then
         TOP+="${ERR_CLR} ${EC}${RESET}\\n"
     else
         TOP+="\\n"
